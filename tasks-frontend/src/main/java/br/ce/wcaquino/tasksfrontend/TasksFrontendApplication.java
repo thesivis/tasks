fb94e1839211db7ffedb1aafad67e22c9d4cc8bc
thesivis <raphael@ic.ufmt.br>
2024-05-28 14:51:18 -0400

    Modifica para rodar localmente

--- tasks-frontend/src/main/java/br/ce/wcaquino/tasksfrontend/TasksFrontendApplication.java
+++ tasks-frontend/src/main/java/br/ce/wcaquino/tasksfrontend/TasksFrontendApplication.java
@@ -2,7 +2,6 @@ package br.ce.wcaquino.tasksfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,11 +9,6 @@ public class TasksFrontendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksFrontendApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksFrontendApplication.class);
 	}
 
 }
